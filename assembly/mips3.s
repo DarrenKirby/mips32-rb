064a2651475b286befeda8c90d8e077860e5ce66
Darren Kirby <bulliver@gmail.com>
2014-09-22 22:58:58 -0600

    Added a 'print statement' ha ha. There is a bug though.

--- assembly/mips3.s
+++ assembly/mips3.s
@@ -3,25 +3,42 @@
 
 .data
 
-fromfile: .space 40
+msg:   .ascii   "In main"
+msg2:  .ascii   "In label1"
+msg3:  .ascii   "In label2"
+msg4:  .ascii   "In exit"
 
 .text
 
 main:
+    li   $v0,4
+    la   $a0,msg
+    syscall
     li $t0,35
     li $t1,456
     li $t2,999
     j label2
 
 label1:
+    li   $v0,4
+    la   $a0,msg2
+    syscall
     li $t3,15
     li $t4,15
     j exit
 
 label2:
+    li   $v0,4
+    la   $a0,msg3
+    syscall
     move $t4,$t5
     li $a2,40
     li $v0,14
     j label1
 
 exit:
+    li   $v0,4
+    la   $a0,msg4
+    syscall
+    li  $v0,10                           # place exit code in $v0
+    syscall                              # call the operating system
